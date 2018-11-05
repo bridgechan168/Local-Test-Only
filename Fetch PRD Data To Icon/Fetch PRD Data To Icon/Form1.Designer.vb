3930483ac9f4901cfe537b59edfea2b1712c771e
318172 <318172@PC1642310>
2018-11-05 13:05:49 +0800

    test

---
+++ Fetch PRD Data To Icon/Fetch PRD Data To Icon/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnStartTimer = New System.Windows.Forms.Button()
+        Me.txtHistory = New System.Windows.Forms.TextBox()
+        Me.btnEndTimer = New System.Windows.Forms.Button()
+        Me.lblRunning = New System.Windows.Forms.Label()
+        Me.SuspendLayout
+        '
+        'btnStartTimer
+        '
+        Me.btnStartTimer.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.btnStartTimer.Location = New System.Drawing.Point(45, 41)
+        Me.btnStartTimer.Name = "btnStartTimer"
+        Me.btnStartTimer.Size = New System.Drawing.Size(208, 41)
+        Me.btnStartTimer.TabIndex = 0
+        Me.btnStartTimer.Text = "Start Timer"
+        Me.btnStartTimer.UseVisualStyleBackColor = true
+        '
+        'txtHistory
+        '
+        Me.txtHistory.Location = New System.Drawing.Point(45, 126)
+        Me.txtHistory.Multiline = true
+        Me.txtHistory.Name = "txtHistory"
+        Me.txtHistory.ReadOnly = true
+        Me.txtHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtHistory.Size = New System.Drawing.Size(517, 480)
+        Me.txtHistory.TabIndex = 1
+        '
+        'btnEndTimer
+        '
+        Me.btnEndTimer.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.btnEndTimer.Location = New System.Drawing.Point(366, 41)
+        Me.btnEndTimer.Name = "btnEndTimer"
+        Me.btnEndTimer.Size = New System.Drawing.Size(196, 41)
+        Me.btnEndTimer.TabIndex = 2
+        Me.btnEndTimer.Text = "End Timer"
+        Me.btnEndTimer.UseVisualStyleBackColor = true
+        '
+        'lblRunning
+        '
+        Me.lblRunning.AutoSize = true
+        Me.lblRunning.Location = New System.Drawing.Point(45, 103)
+        Me.lblRunning.Name = "lblRunning"
+        Me.lblRunning.Size = New System.Drawing.Size(0, 17)
+        Me.lblRunning.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8!, 16!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(612, 650)
+        Me.Controls.Add(Me.lblRunning)
+        Me.Controls.Add(Me.btnEndTimer)
+        Me.Controls.Add(Me.txtHistory)
+        Me.Controls.Add(Me.btnStartTimer)
+        Me.MaximizeBox = false
+        Me.Name = "Form1"
+        Me.Text = "Fetch PRD Data To Mobility"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents btnStartTimer As Button
+    Friend WithEvents txtHistory As TextBox
+    Friend WithEvents btnEndTimer As Button
+    Friend WithEvents lblRunning As Label
+End Class
---
+++ Fetch PRD Data To Icon/Fetch PRD Data To Icon/Form1.vb
@@ -0,0 +1,124 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO
+Imports Newtonsoft.Json
+
+Public Class Form1
+    Dim strConnectionString As String = "Data Source=hxdecoprodbls1;Initial Catalog=hxdb;Integrated Security=SSPI;"
+    Dim WithEvents timerFetch As New Timer()
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        '5 mins
+        timerFetch.Interval = 1000 * 5
+    End Sub
+
+    Private Sub fetchDB(ByVal a_strSql As String, ByVal a_strType As String)
+        Dim dbadpter As SqlDataAdapter
+        Dim dbconn As SqlConnection
+        Dim dsTable As DataSet
+        Dim blnHasData As Boolean = False
+        Dim strRunTime As String = DateTime.Now.ToString("yyyMMddHHmmss")
+
+        Dim objReturn As New Hashtable()
+        objReturn.Add("Type", a_strType)
+
+        Dim strJson As String = ""
+        Dim strFilename As String = a_strType & " " & strRunTime & ".txt"
+        Dim strMsg As String = a_strType
+
+
+        Try
+            dbconn = New SqlConnection(strConnectionString)
+            dbconn.Open()
+            dbadpter = New SqlDataAdapter(a_strSql, dbconn)
+            dsTable = New DataSet()
+            dbadpter.Fill(dsTable)
+
+            If dsTable.Tables(0).Rows.Count > 0 Then
+                blnHasData = True
+
+                Dim arrCols(dsTable.Tables(0).Columns.Count - 1) As String
+                Dim arrData(dsTable.Tables(0).Rows.Count - 1) As Hashtable
+
+                Dim intIndex As Integer = 0
+                For Each dtCol As DataColumn In dsTable.Tables(0).Columns
+                    arrCols(intIndex) = dtCol.ColumnName
+                    intIndex += 1
+                Next
+                'save data
+
+
+                Dim intRowIndex As Integer = 0
+                For intRowIndex = 0 To dsTable.Tables(0).Rows.Count - 1
+                    Dim intColIndex As Integer = 0
+                    Dim objData As New Hashtable()
+                    For intColIndex = 0 To arrCols.Length - 1
+
+                        objData.Add(arrCols(intColIndex), dsTable.Tables(0).Rows(intRowIndex).Item(intColIndex))
+                    Next
+                    arrData(intRowIndex) = objData
+                Next
+                objReturn.Add("Data",arrData)
+                If arrData.Length Then
+
+                    strJson = JsonConvert.SerializeObject(objReturn)
+                    saveToFile(strFilename, strJson)
+                    strMsg = " File:" & strFilename & " saved"
+
+                Else
+                    strMsg &= " data fetched, but cant convert into json"
+                End If
+            End If
+
+            If blnHasData = False Then
+                strMsg &= " no data fetched"
+            End If
+            txtHistory.AppendText(strMsg & vbNewLine)
+            txtHistory.ScrollToCaret()
+            dbconn.Close()
+        Catch ex As Exception
+            Dim strErr As String = ex.Message
+            txtHistory.AppendText(strRunTime & " Exception: " & strErr & vbNewLine)
+            txtHistory.ScrollToCaret()
+        Finally
+            If Not IsNothing(dbadpter) Then dbadpter.Dispose()
+            If Not IsNothing(dsTable) Then dsTable.Dispose()
+            If Not IsNothing(dbconn) Then dbconn.Dispose()
+        End Try
+    End Sub
+
+    Private Function saveToFile(ByVal strFilename As String, ByVal strJson As String) As Boolean
+        Dim strPath As String = "C:\Users\318172\Documents\Icon PRD Data Fetch Routing\From\" & strFilename
+        Try
+            Dim fs As FileStream = File.Create(strPath)
+            Dim arrContent As Byte() = System.Text.Encoding.UTF8.GetBytes(strJson)
+            fs.Write(arrContent, 0, arrContent.Length)
+            fs.Close()
+            Return True
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+    Private Sub btnStartTimer_Click(sender As Object, e As EventArgs) Handles btnStartTimer.Click
+        btnStartTimer.Enabled = False
+        btnEndTimer.Enabled = True
+        lblRunning.Text = "Running..."
+        timerFetch.Start()
+    End Sub
+
+    Private Sub btnEndTimer_Click(sender As Object, e As EventArgs) Handles btnEndTimer.Click
+        btnStartTimer.Enabled = True
+        btnEndTimer.Enabled = False
+        lblRunning.Text = ""
+        timerFetch.Stop()
+    End Sub
+
+    Private Sub timerFetch_Tick(sender As Object, e As EventArgs) Handles timerFetch.Tick
+        Dim strSql As String = "select top 2 * from MCS_MRHeader"
+        fetchDB(strSql, "MR")
+    End Sub
+End Class
